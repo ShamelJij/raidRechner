61f0454d65b6fe87172bfb6963a64a478367dd89
shamel jij <shamel.jij@gmail.com>
2022-05-24 12:24:48 +0200

    chart is added

--- app.js
+++ app.js
@@ -2,12 +2,38 @@ let kapazitaeten = [500, 1000, 2000, 5000, 10000];
 let kText = ["500GB", "1TB", "2TB", "5TB", "10TB"];
 let anzahl = document.getElementById("anzahl");
 let kapazitaet = document.getElementById("kapazitaet");
-
-//kapazitaet[slider.value];
+let nichtErlaubt = [[]]
+let effizienz;
+let rest;
 
 function erfassen(){
     document.getElementById("anzahlText").innerHTML = anzahl.value;
     document.getElementById("kapazitaetText").innerHTML = kText[kapazitaet.value];
+    document.getElementById("anzahlText").classList = "text-dark";
+    switch (document.getElementById("level").value){
+        case "1":
+            if(anzahl.value % 2 != 0){
+                document.getElementById("anzahl").value = Math.round(document.getElementById("anzahl").value*0.5)*2;
+                document.getElementById("anzahlText").value  += Math.round(document.getElementById("anzahl").value*0.5)*2;
+            }
+        break;
+        case "5":
+            if(anzahl.value <= 3){
+                document.getElementById("anzahlText").classList = "text-danger";
+            }
+        break;
+        case "6":
+            if(anzahl.value <= 4){
+                document.getElementById("anzahlText").classList = "text-danger";
+            }
+        break;
+        case "10":
+            if(anzahl.value % 2 != 0){
+                document.getElementById("anzahlText").classList = "text-danger";
+            }
+        break;
+        default:
+    }
 }
 
 function ausrechnen(){
@@ -40,7 +66,7 @@ function ausrechnen(){
             netto = brutto/2;
         break;
         default:
-            netto = "jBoD";
+            netto = brutto;
     }
         if(netto > 0 )
         {
@@ -48,7 +74,47 @@ function ausrechnen(){
         }else{
             document.getElementById("netto").innerHTML = "Nicht genügend Platten!";
         }
-    let effizienz = netto/brutto*100;
+    effizienz = netto/brutto*100;
     effizienz = effizienz.toFixed(2);
-    document.getElementById("effizienz").innerHTML = effizienz;
+    document.getElementById("effizienz").innerHTML = effizienz +" %";
+    rest = 100-effizienz;
+    console.log(rest);
+}
+
+// Canvas Chart
+function chart(){
+    var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ['Effizient', 'nicht verwendet'],
+    datasets: [{
+      data: [effizienz, rest],
+      backgroundColor: [
+        'rgba(41, 121, 255, 1)',
+        'rgba(38, 198, 218, 1)',
+        'rgba(138, 178, 248, 1)',
+        'rgba(255, 100, 200, 1)',
+        'rgba(116, 96, 238, 1)',
+        'rgba(215, 119, 74, 1)',
+        'rgba(173, 92, 210, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(247, 247, 247, 1)',
+        'rgba(227, 247, 227, 1)',
+      ],
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    cutoutPercentage: 80,
+    tooltips: {
+      callbacks: {
+        label: function(tooltipItem, data) {
+          return data['labels'][tooltipItem['index']] + ': ' + data['datasets'][0]['data'][tooltipItem['index']] + '%';
+        }
+      }
+    }
+  }
+});
 }
